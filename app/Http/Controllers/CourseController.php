8af4ba452509c951b19b172de245721b5b8c015a
arudovwen <successahon@gmail.com>
2021-05-06 18:07:17 +0100

    update

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -60,7 +60,6 @@ class CourseController extends Controller
         ]);
     }
 
-
     public function show(Course $course)
     {
         return Course::with('curriculum')->with('module')->with('feedback')->where('id', $course->id)->first();
