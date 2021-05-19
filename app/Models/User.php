25ea8aa88b1f83abba78ab28f2d5d572519f898c
arudovwen <successahon@gmail.com>
2021-05-19 15:20:07 +0100

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -38,7 +38,7 @@ class User extends Authenticatable
     }
     public function loginhistory()
     {
-        return $this->hasMany(loginhistory::class);
+        return $this->hasMany(LoginHistory::class);
     }
     public function inbox()
     {
